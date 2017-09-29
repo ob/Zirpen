1be35e2ce58ce3596fb3b5c7b8af7156ccd5d03e
Oscar Bonilla <obonilla@linkedin.com>
2017-09-28 17:14:25 -0700

    Start implementing the tweet functionality

--- Zirpen/Controllers/ComposeTweetController.swift
+++ Zirpen/Controllers/ComposeTweetController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class ComposeTweetController: UIViewController {
+class ComposeTweetController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var tweetTextField: UITextView!
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.size.width / 2
+        avatarImageView.clipsToBounds = true
+        avatarImageView.layer.borderWidth = 1.0
+        avatarImageView.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+
+        if let user = User.currentUser,
+            let url = user.profileURL {
+            avatarImageView.setImageWith(url)
+        }
+        countLabel.text = "140"
+        tweetTextField.delegate = self
+        tweetTextField.becomeFirstResponder()
         // Do any additional setup after loading the view.
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let newLength = (textView.text?.count ?? 0) + text.count - range.length
+        updateCountLabel(newLength)
+        return true
+    }
+    
+    func updateCountLabel(_ length: Int) {
+        let remaining = 140 - length
+        if remaining < 0 {
+            countLabel.textColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        } else {
+            countLabel.textColor = #colorLiteral(red: 0.5723067522, green: 0.5723067522, blue: 0.5723067522, alpha: 1)
+        }
+        countLabel.text = String(remaining)
     }
 
     override func didReceiveMemoryWarning() {
